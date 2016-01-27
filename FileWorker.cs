a376813799b9c331582503d572641853e6ec0e2f
YukiNyaa712 <yukinyaa712@gmail.com>
2016-01-28 01:46:36 +0900

    Initial
    I started git :+1:

---
+++ FileWorker.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+namespace WindowsFormsApplication2
+{
+    class FileWorker
+    {
+        /*
+         * back up/load these
+         * C:\Users\(username)\AppData\Local\Google\Chrome\User Data\Default\IndexedDB\chrome-extension_hkgmldnainaglpjngpajnnjfhpdjkohh_0.indexeddb.leveldb
+         * C:\Users\(username)\AppData\Local\Google\Chrome\User Data\Default\Local Storage\chrome-extension_hkgmldnainaglpjngpajnnjfhpdjkohh_0.localstorage(file)
+         * C:\Users\(username)\AppData\Local\Google\Chrome\User Data\Default\Managed Extension Settings\hkgmldnainaglpjngpajnnjfhpdjkohh
+         * C:\Users\(username)\AppData\Local\Google\Chrome\User Data\Default\Sync Extension Settings\hkgmldnainaglpjngpajnnjfhpdjkohh
+         * C:\Users\(username)\AppData\Local\Google\Chrome\User Data\Default\Local Extension Settings\hkgmldnainaglpjngpajnnjfhpdjkohh
+         * */
+        static private String userName = System.Environment.UserName;
+        static private String chromeUserDir;// aka C:\Users\(username)\AppData\Local\Google\Chrome\User Data
+        static private String TmpDataDir;
+        static private String appname = "hkgmldnainaglpjngpajnnjfhpdjkohh";
+
+
+        static public void setUserName(String username_)
+        {
+            userName = username_;
+            setChromeDataDirectory();
+        }
+        static public void setChromeDataDirectory(String overrideDirectory=null)
+        {
+            if (overrideDirectory != null)
+            {
+                chromeUserDir = overrideDirectory;
+            }
+            else
+                chromeUserDir = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                        @"AppData\Local\Google\Chrome\User Data"
+                    );
+        }
+        static void setTmpDataDirectory()
+        {
+            TmpDataDir = Path.Combine(Path.GetTempPath(),"kc3back");
+            try {
+                Directory.Delete(TmpDataDir, true);
+            }
+            catch (DirectoryNotFoundException dnf) { };
+            
+            Directory.CreateDirectory(TmpDataDir);
+        }
+
+        static public void backup()
+        {
+            setTmpDataDirectory();
+            Copy.directory(
+                Path.Combine(chromeUserDir,@"Default\IndexedDB\chrome-extension_hkgmldnainaglpjngpajnnjfhpdjkohh_0.indexeddb.leveldb"),
+                TmpDataDir
+                );
+            Copy.directory(
+                Path.Combine(chromeUserDir, @"Default\Managed Extension Settings\hkgmldnainaglpjngpajnnjfhpdjkohh"),
+                TmpDataDir
+                );
+            Copy.directory(
+                Path.Combine(chromeUserDir, @"Default\Sync Extension Settings\hkgmldnainaglpjngpajnnjfhpdjkohh"),
+                TmpDataDir
+                );
+            Copy.directory(
+                Path.Combine(chromeUserDir, @"Default\Local Extension Settings\hkgmldnainaglpjngpajnnjfhpdjkohh"),
+                TmpDataDir
+                );
+            Copy.file(
+                Path.Combine(chromeUserDir, @"Default\Local Storage\chrome-extension_hkgmldnainaglpjngpajnnjfhpdjkohh_0.localstorage"),
+                TmpDataDir
+                );
+            ZipFile.CreateFromDirectory(TmpDataDir, getSaveDir());
+            setTmpDataDirectory();
+        }
+
+        static String getSaveDir()
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "zip files (*.zip)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            while (saveFileDialog1.ShowDialog() == DialogResult.OK) ;
+            return saveFileDialog1.FileName;
+        }
+
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Username = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.Override = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(3, 3);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(100, 21);
+            this.Username.TabIndex = 1;
+            this.Username.Text = "Username";
+            // 
+            // Override
+            // 
+            this.Override.Location = new System.Drawing.Point(140, 3);
+            this.Override.Name = "Override";
+            this.Override.Size = new System.Drawing.Size(75, 23);
+            this.Override.TabIndex = 3;
+            this.Override.Text = "OverRide";
+            this.Override.UseVisualStyleBackColor = true;
+            this.Override.Click += new System.EventHandler(this.Override_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "do not touch this if this user\'s data";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.Username);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Override);
+            this.panel1.Location = new System.Drawing.Point(330, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 193);
+            this.panel1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Chrome Data DIrectory";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "OverRide";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Backup";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(184, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Restore";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = " or chrome is on other directory";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "*\\(Google)\\Chrome\\User Data";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Username;
+        public System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button Override;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
